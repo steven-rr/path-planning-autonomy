62aa1a14e1bd9b892345e3d190e8676f3e49e12b
steven-rr <steverelativity@gmail.com>
2020-10-10 05:32:58 -0400

    p3 solved.

--- hw2/p3_astar.py
+++ hw2/p3_astar.py
@@ -8,12 +8,18 @@ graph1 = {1:[2,16],       2:[1,3],          3:[2,4,14],    4:[3,5,13],        5:
           41:[40,42,56], 42:[39,41,43,55], 43:[38,42,44], 44:[37,43,45,53],  45:[44,46,52],    46:[35,45,47,51], 47:[34,46,48,50], 48:[33,47],
           49:[],         50:[47,51],       51:[46,50,52], 52:[45,51,53],     53:[44,52],       54:[],            55:[42,56],       56:[41,55]}
 
-print(graph1[1])
+heuristic_cost = { 1:12,    2:11,    3:10,    4:9,     5:8,     6:100,  7:6,     8:7,
+                   9:6,    10:5,    11:100,  12:7,    13:8,    14:9,   15:100,  16:11,
+                  17:10,   18:100,  19:8,    20:7,    21:6,    22:5,   23:4,    24:5,
+                  25:100,  26:3,    27:4,    28:100,  29:100,  30:7,   31:8,    32:9,
+                  33:8,    34:7,    35:6,    36:100,  37:4,    38:3,   39:2,    40:1,
+                  41:0,    42:1,    43:2,    44:3,    45:4,    46:5,   47:6,    48:7,
+                  49:100,  50:7,    51:6,    52:5,    53:4,    54:100, 55:2,    56:1}
 
 #perform Astar search on the graph.
-Q = []
+Q = [[[1],12]]
 expanded = []
-
+i = 0
 while True:
     # step 2: if Q is empty, fail, else choose best path from Q.
     if  Q[0] == []:
@@ -26,7 +32,7 @@ while True:
     best_path = Q[best_path_index][0];
     best_cost = Q[best_path_index][1] - heuristic_cost[current_head];
     # step 3 and 4: if head is G, return the best partial path. else remove the best partial path.
-    if current_head == 'G':
+    if current_head == 41:
         best_path_result = Q[best_path_index]
         break
     else:
@@ -40,14 +46,18 @@ while True:
         expanded.append(current_head)
         children = graph1[current_head]
         for child in children:
-            if child[0] in expanded:
+            if child in expanded:
                 children.remove(child)
         for child in children:
-            new_path = child[0] + ' ' + best_path
-            new_cost = best_cost + child[1] + heuristic_cost[child[0]]
+            new_path = best_path[:]
+            new_path.insert(0,child)
+            new_cost = best_cost + 1 + heuristic_cost[child]
             Q.append([new_path, new_cost])
 
     #prevent overload.
     i = i + 1
-    if i > 100:
+    if i > 10000:
         break
+
+print("best path is: ", best_path_result[0])
+print("best path cost is: ", best_path_result[1])
