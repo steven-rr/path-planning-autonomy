29b068e4225f549e1d9ac7e220d0c9f8fbe49434
steven-rr <steverelativity@gmail.com>
2020-10-15 10:52:50 -0400

    p5 solved.

--- hw2/p5_8puzzle.py
+++ hw2/p5_8puzzle.py
@@ -2,21 +2,18 @@ from p6_astar import move
 from copy import deepcopy
 
 
-
-def main():
-    list_init_state = [1,2,3,4,5,6,7,8,0]
-
+def solve_all_possible_states(list_init_state):
     graph1 = {}
-    graph_nodes = {1:list_init_state}
+    graph_nodes = {1: list_init_state}
 
-    #counter to break.
+    # counter to break.
     counter = 100;
     Q = [[1]]
     while True:
         parents = deepcopy(Q)
         # loop thru parents. add children as nodes, and into graph1. then delete parents.
-        #[[4, 2, 1], [5, 2, 1], [6, 3, 1], [7, 3, 1]]
-        for i in range(0,len(parents)):
+        # [[4, 2, 1], [5, 2, 1], [6, 3, 1], [7, 3, 1]]
+        for i in range(0, len(parents)):
             parent_head = parents[i][0]
             parent_path = deepcopy(parents[i])
             children = move(graph_nodes[parent_head])
@@ -45,8 +42,29 @@ def main():
 
         print("nodes,expanded: ", len(graph_nodes.values()))
         if Q == []:
-            break
-            print("ended.")
+            print("Solving for all states : Complete.")
+            print("Number of nodes expanded: " ,len(graph_nodes))
+            return graph_nodes
+def main():
+
+    # 1 2 3
+    # 4 5 6
+    # 7 8 0
+    list_init_state = [1,2,3,4,5,6,7,8,0]
+    graph_nodes_even = solve_all_possible_states(list_init_state)
+    # 1 2 3
+    # 8 0 4
+    # 7 6 5
+    list_init_state = [1,2,3,8,0,4,7,6,5]
+    graph_nodes_odd = solve_all_possible_states(list_init_state)
+
+
+    if graph_nodes_even[1] in graph_nodes_odd.values():
+        disjoint = False
+    else:
+        disjoint = True
+    print("If disjoint is true, it is proof that there are 2 disjoint sets for the 8 puzzle: ")
+    print(disjoint)
     return 0
 
 
